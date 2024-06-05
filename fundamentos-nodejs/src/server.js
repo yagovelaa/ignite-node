3effbc0aa43d88a65fbb87b46405ea2a9f9f1975
yagovelaa <yagovela@outlook.com>
2024-06-05 15:12:29 -0300

    Fundamentos node: conhecendo HTTP status code

--- fundamentos-nodejs/src/server.js
+++ fundamentos-nodejs/src/server.js
@@ -15,9 +15,11 @@ const server = http.createServer((req, res) => {
             name: 'John Doe',
             email: 'johndoe@example.com'
         })
+
+        return res.writeHead(201).end()
     }
 
-    return res.end('Hello world')
+    return res.writeHead(404).end('Not found')
 })
 
 server.listen(3333)
