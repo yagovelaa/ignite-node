e599828fec13403996e2314217d812c93f7ac95e
yagovelaa <yagovela@outlook.com>
2024-06-05 14:51:22 -0300

    Fundamentos node: rotas de criação e listagem (Métodos HTTP)

--- fundamentos-nodejs/src/server.js
+++ fundamentos-nodejs/src/server.js
@@ -1,6 +1,16 @@
 import http from 'http'
 
 const server = http.createServer((req, res) => {
+    const { method, url} = req
+
+    if(method === 'GET' && url === "/users") {
+        return res.end('users list')
+    }
+
+    if(method === 'PORT' && url === "/users") {
+        return res.end('users create')
+    }
+
     return res.end('Hello world')
 })
 
