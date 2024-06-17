aab9870ebd59000cdb7c421eaa9604720632017f
yagovelaa <yagovela@outlook.com>
2024-06-17 11:46:51 -0300

    Feature: Configuring Knex

--- 02-api-rest-nodejs/src/server.ts
+++ 02-api-rest-nodejs/src/server.ts
@@ -1,7 +1,13 @@
-import fastify from "fastify"
-
+import fastify from 'fastify'
+import { knex } from './database'
 
 const app = fastify()
+
+app.get('/hello', async () => {
+    const tables = await knex('sqlite_schema').select('*')
+  
+    return tables
+  })
 
 app.listen({    
     port: 3333,
