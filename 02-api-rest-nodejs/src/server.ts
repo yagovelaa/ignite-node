76a9547e959dcd40c3c7d467ef34134c750ec591
yagovelaa <yagovela@outlook.com>
2024-06-20 16:04:05 -0300

    Feature: Fastify Plugins

--- 02-api-rest-nodejs/src/server.ts
+++ 02-api-rest-nodejs/src/server.ts
@@ -1,14 +1,10 @@
 import fastify from 'fastify'
-import { knex } from './database'
 import { env } from './env'
+import { transactionsRoutes } from './routes/transactions'
 
 const app = fastify()
 
-app.get('/hello', async () => {
-    const transaction = await knex('transactions').select('*')
-
-    return transaction
-  })
+app.register(transactionsRoutes)
 
 app.listen({    
     port: env.PORT,
