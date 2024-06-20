9cd25a60998ad80f8a22b41da2c23b06ce14373f
yagovelaa <yagovela@outlook.com>
2024-06-20 16:28:07 -0300

    Feature: Transaction creation

--- 02-api-rest-nodejs/src/routes/transactions.ts
+++ 02-api-rest-nodejs/src/routes/transactions.ts
@@ -1,10 +1,24 @@
 import { FastifyInstance } from "fastify"
+import { randomUUID } from 'node:crypto'
+import { z } from "zod"
 import { knex } from "../database"
 
 export async function transactionsRoutes(app: FastifyInstance) {
-    app.get('/hello', async () => {
-        const transaction = await knex('transactions').select('*')
+    app.post('/', async (request, reply) => {
+        const createTransactionBodySchema = z.object({
+            title: z.string(),
+            amount: z.number(),
+            type: z.enum(['credit', 'debit']),
+        })
+
+        const { title, amount, type } = createTransactionBodySchema.parse(request.body)
+
+        await knex('transactions').insert({
+            id: randomUUID(),
+            title,
+            amount: type === 'credit' ? amount : amount * -1,
+        })
     
-        return transaction
+        return reply.status(201).send()
     })
 }
