d6f60792471931d486226e42c57f716ab74fd145
yagovelaa <yagovela@outlook.com>
2024-06-20 19:58:50 -0300

    Feature: Using cookies in Fastify

--- 02-api-rest-nodejs/src/routes/transactions.ts
+++ 02-api-rest-nodejs/src/routes/transactions.ts
@@ -37,12 +37,24 @@ export async function transactionsRoutes(app: FastifyInstance) {
             type: z.enum(['credit', 'debit']),
         })
 
+        let sessionId = request.cookies.sessionId
+
+        if(!sessionId) {
+            sessionId = randomUUID()
+
+            reply.cookie('sessionId', sessionId, {
+                path: '/',
+                maxAge: 60 * 60 * 24 * 7, // 7 days
+            })
+        }
+
         const { title, amount, type } = createTransactionBodySchema.parse(request.body)
 
         await knex('transactions').insert({
             id: randomUUID(),
             title,
             amount: type === 'credit' ? amount : amount * -1,
+            session_id: sessionId
         })
     
         return reply.status(201).send()
