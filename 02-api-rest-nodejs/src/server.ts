b1983e18647f496dcb05b7649da28bfe3c1471de
yagovelaa <yagovela@outlook.com>
2024-06-26 16:33:38 -0300

    Feature: Testing transaction creation

--- 02-api-rest-nodejs/src/server.ts
+++ 02-api-rest-nodejs/src/server.ts
@@ -1,17 +1,10 @@
-import cookie from '@fastify/cookie'
-import fastify from 'fastify'
-import { env } from './env'
-import { transactionsRoutes } from './routes/transactions'
+import { app } from "./app";
+import { env } from "./env";
 
-const app = fastify()
-
-app.register(cookie)
-app.register(transactionsRoutes, {
-    prefix: 'transactions',
-})
-
-app.listen({    
+app
+  .listen({
     port: env.PORT,
-}).then(() => {
-    console.log('HTTP Server Running!')
-})
+  })
+  .then(() => {
+    console.log("HTTP Server Running!");
+  });
