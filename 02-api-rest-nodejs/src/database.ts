486728ac795c5017e0f953af3484485f4bdce77a
yagovelaa <yagovela@outlook.com>
2024-06-28 11:41:26 -0300

    Chore: prodution config

--- 02-api-rest-nodejs/src/database.ts
+++ 02-api-rest-nodejs/src/database.ts
@@ -1,16 +1,19 @@
-import { Knex, knex as setupKnex } from 'knex'
-import { env } from './env'
+import { Knex, knex as setupKnex } from "knex";
+import { env } from "./env";
 
 export const config: Knex.Config = {
-  client: 'sqlite',
-  connection: {
-    filename: env.DATABASE_URL,
-  },
+  client: env.DATABASE_CLIENT,
+  connection:
+    env.DATABASE_CLIENT === "sqlite"
+      ? {
+          filename: env.DATABASE_URL,
+        }
+      : env.DATABASE_URL,
   useNullAsDefault: true,
   migrations: {
     extension: "ts",
-    directory: "./db/migrations"
-  }
-}
+    directory: "./db/migrations",
+  },
+};
 
-export const knex = setupKnex(config)
+export const knex = setupKnex(config);
